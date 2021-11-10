3e9a78333ab51934033fbfc0f36db6ffc51eb08b
Eganathan R <ammasamrith@gmail.com>
2021-11-10 13:47:38 +0530

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -1,21 +1,31 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
+
 
 public class MyFrame extends JFrame implements ActionListener {
 
-    private final JButton addImgBtn;
-    private final JPanel centerPanel;
-    private JLabel memIme;
-    private ImageIcon memIco;
+    private final JButton addImgBtn; //Image ADD Button
+    private final JPanel centerPanel; //Center Panel
+    private JLabel memIme; //JLabel Center Image
+    private ImageIcon memIco;//Image icon
+    private String userFile; //File details
+    private JLabel userInputLbl; //Meme Text label
+    private JTextField userInput;//User Input field
+
+    //Swatch BUTTONS
+    private JButton fontRedBtn;
+    private  JButton fontBlueBtn;
+    private  JButton fontGreenBtn;
 
 
 
     public MyFrame(){
         //MAIN FRAME
         JFrame mainFrame = new JFrame("MEME-Gen V0.01");
+
 
         //Top Pane or NORTH PANE
         JPanel topPanel = new JPanel();
@@ -30,52 +40,57 @@ public class MyFrame extends JFrame implements ActionListener {
         JLabel footerLbl = new JLabel("FOOTER LABEL");
         btmPanel.add(footerLbl);
         mainFrame.add(btmPanel);
-
-        //WEST- Option side bar
+        //WEST- EDIT SIDE BAR
         JPanel westPanel = new JPanel(); //The West EDit panel
-        westPanel.setLayout(new GridLayout(8,1,2,2));
+        westPanel.setBackground(Color.BLACK);
+        westPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); //Empty margin for margin
+        westPanel.setLayout(new GridLayout(10,1,2,2)); //layount rows and columns
         JLabel westLbl = new JLabel("Editing PANEL"); //label to identify panel
         JLabel btndesLbl = new JLabel("Add an image");
         JLabel inputdesLbl = new JLabel("Text color");
-
+        //FONT SWATCH
         JPanel colorSwatch = new JPanel();
         colorSwatch.setLayout(new GridLayout(1,3));
-        JButton redBtn = new JButton();
-        redBtn.setBackground(Color.RED);
-        JButton blueBtn = new JButton();
-        blueBtn.setBackground(Color.BLUE);
-        JButton greenBtn = new JButton();
-        greenBtn.setBackground(Color.GREEN);
-        colorSwatch.add(redBtn);
-        colorSwatch.add(blueBtn);
-        colorSwatch.add(greenBtn);
+        fontRedBtn = new JButton();
+        fontRedBtn.setBackground(Color.RED);
+        fontBlueBtn = new JButton();
+        fontBlueBtn.setBackground(Color.BLUE);
+        fontGreenBtn = new JButton();
+        fontGreenBtn.setBackground(Color.GREEN);
+        colorSwatch.add(fontRedBtn);
+        colorSwatch.add(fontBlueBtn);
+        colorSwatch.add(fontGreenBtn);
 
         addImgBtn = new JButton("SELECT IMAGE"); //Button to upload user image
         addImgBtn.addActionListener(this);// adds the object name to the action listener so a file choose pops up
         JButton saveImgBtn = new JButton("SAVE IMAGE"); //Button to upload user image
-        JTextField topInput = new JTextField();
-        //topInput.setMinimumSize();
+        userInput = new JTextField(1); //setting max column
+        userInput.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        westPanel.add(westLbl);// adding the identification label
+        //westPanel.add(westLbl);// adding the identification label
         westPanel.add(btndesLbl);
         westPanel.add(addImgBtn); // adding the button to
         westPanel.add(inputdesLbl);
         westPanel.add(colorSwatch);
-        westPanel.add(topInput);
+        westPanel.add(userInput);
         westPanel.add(saveImgBtn);
+        //END OF EDIT PANEL
 
-
-        //CENTER Canvas Option
+        //CENTER PANEL
         centerPanel = new JPanel();
         centerPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        JLabel eastLbl = new JLabel("CENTER PANEL");
+        centerPanel.setBackground(Color.BLACK);
+        userInputLbl = new JLabel("Aren't you typing anything ?");
+        userInputLbl.setFont(new Font("Serif", Font.PLAIN, 40));
+        userInputLbl.setForeground(Color.CYAN);
 
-        memIco = new ImageIcon(getClass().getResource("MEMGEN_icon.png"));
+
         memIme = new JLabel(memIco);
         memIme.setSize(centerPanel.getWidth(),centerPanel.getWidth()-100);
 
-        centerPanel.add(eastLbl);
-        centerPanel.add(memIme);
+        centerPanel.setLayout(new BorderLayout());
+        centerPanel.add(userInputLbl,BorderLayout.SOUTH);
+        centerPanel.add(memIme,BorderLayout.NORTH);
 
 
 
@@ -99,19 +114,35 @@ public class MyFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == addImgBtn){
-            JFileChooser file = new JFileChooser();
+            userInputLbl.setText(userInput.getText());
+            JFileChooser file = new JFileChooser("/Desktop");
+            file.setFileFilter(new FileNameExtensionFilter("JPEG file", "jpg", "jpeg"));
             file.showOpenDialog(null);
-            System.out.println("Working!!");
+            userFile =file.getSelectedFile().getPath();
+            if(userFile != ""){
 
-            String path=file.getSelectedFile().getAbsolutePath();
-            String filename=file.getSelectedFile().getName();
+                memIco = new ImageIcon(new ImageIcon(userFile).getImage().getScaledInstance(centerPanel.getWidth(), centerPanel.getHeight()-50, Image.SCALE_DEFAULT));//new ImageIcon();
+                memIme.setIcon(memIco);
+                centerPanel.updateUI();
+            }else{
+                JOptionPane.showMessageDialog(null, "Feel Free to Look Later");
+            }
 
-        } else{
-            System.out.println("NOT Working!!");
+
+            centerPanel.add(memIme,BorderLayout.NORTH);
+            userInputLbl.setText(userInput.getText());
+            centerPanel.updateUI();
+
+        }else if(e.getSource() == fontBlueBtn){
+
+            userInputLbl.setForeground(Color.BLUE);
+            userInputLbl.repaint();
+            userInputLbl.setBackground(Color.RED);
+            centerPanel.updateUI();
+            System.out.println("wrong!!");
         }
-    }
-
-    public void paid(Graphics g){
-
+        else{
+            System.out.println("NIl wrong!!");
+        }
     }
 }
